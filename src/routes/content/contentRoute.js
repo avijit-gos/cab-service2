b750f2093c839714abefa4580ac529cb5c0bb7db
Soumik-Git-One <soumik@onenesstechs>
2024-05-31 18:11:05 +0530

    final commit

--- src/routes/content/contentRoute.js
+++ src/routes/content/contentRoute.js
@@ -1,11 +1,12 @@
 const router = require("express").Router();
-const {createContent, getAboutUs,updateAboutStatus, getPrivacy, updatePrivacyStatus, updateContent} = require("../../controller/content/contentController");
+const {createContent, getAboutUs, getPrivacy,updatestatus, updateContent, getAllContents} = require("../../controller/content/contentController");
 const verifyAdmin = require("../../middleware/verifyAdmin");
 
 router.post("/", createContent);
 router.get("/about", getAboutUs);
 router.get("/privacy", getPrivacy);
-router.put("/update-about-status", updateAboutStatus);
-router.put("/update-privacy-status", updatePrivacyStatus);
-router.put("/update-content/:id", updateContent)
+router.put("/update-status/:id", updatestatus);
+router.put("/update-privacy-status", updatestatus);
+router.put("/update-content/:id", updateContent);
+router.get("/", getAllContents)
 module.exports = router;
